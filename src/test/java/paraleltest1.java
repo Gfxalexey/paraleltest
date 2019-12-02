40964064de41a2f961fee69d997403337ae3155c
Gfxalexey <55746754+Gfxalexey@users.noreply.github.com>
2019-12-02 01:29:27 -0800

    Update paraleltest1.java

--- src/test/java/paraleltest1.java
+++ src/test/java/paraleltest1.java
@@ -16,32 +16,12 @@ import java.util.concurrent.TimeUnit;
 
 public class paraleltest1 extends BaseTest {
 
-//    public AppiumDriver<MobileElement> driver;
-//    @Parameters({"device","appiumServer"})
-//    @BeforeClass
-//    public void setup(String device,String appiumServer) throws MalformedURLException {
-//        DesiredCapabilities cap = new DesiredCapabilities();
-//        cap.setCapability(MobileCapabilityType.DEVICE_NAME,device);
-//        cap.setCapability(MobileCapabilityType.UDID,device);
-//        cap.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 120);
-//        cap.setCapability(AndroidMobileCapabilityType.APP_PACKAGE,"com.ideomobile.discount");
-//        cap.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY,"com.discount.ui.DiscountSplashActivity");
-//        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-//        cap.setCapability(MobileCapabilityType.VERSION,9);
-//        URL url = new URL(appiumServer);
-//        driver = new AppiumDriver<MobileElement>(url,cap);
-//        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-//    }
 
     @Test
     public void login() throws InterruptedException {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.EnterButton.click();
-        Thread.sleep(1000);
-        loginPage.loginsucces("320555683", "1q1q1q", "1q1q1q");
-        Thread.sleep(10000); //fgdfg
-        driver.navigate().back();
-        Thread.sleep(3000);
+       
 
     }
 
